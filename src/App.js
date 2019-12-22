202c434e3368a1bd4d942a6b864873c64e0585fc
Adam Gray <adamjamesgray.1992@gmail.com>
2019-12-22 15:55:35 +0000

    Split code into components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { CardList } from './components/CardList.component';
 
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users').then(response =>
+			response.json().then(users => this.setState({ monsters: users }))
+		);
+	}
+
+	render() {
+		return (
+			<CardList>
+				{this.state.monsters.map(monster => (
+					<h2 key={monster.id}>{monster.name}</h2>
+				))}
+			</CardList>
+		);
+	}
+}
 export default App;
